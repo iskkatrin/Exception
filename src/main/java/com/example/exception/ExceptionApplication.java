ac1daf24b737cfde86d236ecca6bedc686e9fb87
Ekaterina <klarina2409@gmail.com>
2023-10-09 23:12:44 +0300

    HW

--- src/main/java/com/example/exception/ExceptionApplication.java
+++ src/main/java/com/example/exception/ExceptionApplication.java
@@ -5,18 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExceptionApplication {
-	public static void main(String[] args) {
-		String login = "java_skypro_go";
-		String password = "D_1hWiKjjP_9";
-		String confirmPassword = "D_1hWiKjjP_9";
+    public static void main(String[] args) {
+        String login = "java_skypro_go";
+        String password = "D_1hWiKjjP_9";
+        String confirmPassword = "D_1hWiKjjP_9";
 
-		try {
-			UserRegistrationUtils.registerUser(login, password, confirmPassword);
-			System.out.println("Пользователь успешно зарегистрирован");
-		} catch (WrongLoginException e) {
-			System.out.println("Ошибка регистрации: " + e.getMessage());
-		} catch (WrongPasswordException e) {
-			System.out.println("Ошибка регистрации: " + e.getMessage());
-		}
-	}
+        try {
+            UserRegistrationUtils.registerUser(login, password, confirmPassword);
+            System.out.println("Пользователь успешно зарегистрирован");
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка регистрации: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка регистрации: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/exception/UserRegistrationUtils.java
+++ src/main/java/com/example/exception/UserRegistrationUtils.java
@@ -14,4 +14,5 @@ public class UserRegistrationUtils {
             throw new WrongPasswordException("Пароли не совпадают");
         }
     }
+
 }
